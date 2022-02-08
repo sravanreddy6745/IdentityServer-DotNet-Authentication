896e814239ecab8158c968751a1a9c033d7cbefe
Sravan <satya.penmatsa@elevatelearning.com>
2022-02-08 12:56:32 +0530

    initial-commit

---
+++ AuthenticationModule/Models/ErrorDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationModule.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ AuthenticationModule/Models/ServiceExceptionInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationModule.Models
+{
+    public class ServiceExceptionInterceptor : ExceptionFilterAttribute, IExceptionFilter
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var error = new ErrorDetails()
+            {
+                StatusCode = 500,
+                Message = "Something went wrong! Internal Server Error."
+            };
+
+        //Logs your technical exception with stack trace below
+
+        context.Result = new JsonResult(error);
+    }
+
+}
+}
